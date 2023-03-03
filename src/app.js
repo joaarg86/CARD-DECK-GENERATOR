e2d13fe6adefb56e72e1277a777b4a883d4c891d
joaarg86 <johanna.e.escobar@icloud.com>
2023-03-03 23:29:27 +0000

    new

--- src/app.js
+++ src/app.js
@@ -1,11 +1,63 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+/*function generateCard() {}*/
+/* document.querySelector(".card").classList.add("generateRandomSuit");
+  document.querySelector(".card").innerHTML.add("generateRandomNumbers");
+
+  let generateRandomNumbers = () => {
+    let numbers = ["8", "3", "7", "9", "6", "4", "A", "5", "K", "Q", "2", "J"];
+    let indexNumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[indexNumbers];
+  };
+  let generateRandomSuit = () => {
+    let suit = ["♦", "♥", "♠", "♣"];
+    let indexSuit = Math.floor(Math.random() * suit.length);
+    return suit[indexSuit];
+  };
+};*/
+//document.getElementById().innerHTML = generateCard()
+
+function getSuit() {
+  let suit = ["♦", "♥", "♠", "♣"];
+  let suIndex = Math.floor(Math.random() * suit.length);
+  return suit[suIndex];
+}
+
+function getNumber() {
+  let number = [
+    "8",
+    "3",
+    "7",
+    "9",
+    "6",
+    "4",
+    "A",
+    "5",
+    "K",
+    "Q",
+    "2",
+    "J",
+    "10"
+  ];
+
+  let numIndex = Math.floor(Math.random() * number.length);
+  return number[numIndex];
+}
+window.onload = () => {
+  document.getElementById("number").innerText = getNumber();
+  let s = getSuit();
+  document.getElementById("top-suit").innerText = s;
+  document.getElementById("bottom-suit").innerText = s;
+  if (s == "♥" || s == "♦") {
+    document.getElementById("top-suit").classList.add("red");
+    document.getElementById("bottom-suit").classList.add("red");
+    document.getElementById("number").classList.add("red");
+  }
+
+  // let result;
+  // " " + suit[suIndex] + " " + number[numIndex];
 };
